4fcdef44b309fc89d644ae6491438871e843a79a
Ezequiel Tejeda <eze23-96@hotmail.com>
2021-12-09 13:10:42 -0300

    cambios de estilo interfaz de admin

--- src/main/resources/static/web/scripts/modificacion-productos.js
+++ src/main/resources/static/web/scripts/modificacion-productos.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
         return {
             modificarOAgregarProducto: "",
             nombreProducto: "",
+            descripcionProducto: "",
             marca: "",
             costo: 0,
             porcentajeGanancia: 0,
@@ -10,8 +11,8 @@ const app = Vue.createApp({
             tallesProducto: [],
             equipoProducto: "",
             tipoProducto: "",
-            imagenProducto: "",
-            descripcionProducto: ""
+            imagenProducto: ""
+            
         }
     },
     created() {
@@ -22,29 +23,81 @@ const app = Vue.createApp({
     },
     methods: {
         guardarProducto() {
-            axios.post("/api/productos",{ "name": this.nombreProducto,
-                                            "nombreProducto": this.descripcionProducto,
-                                            "precioCosto": this.costo, 
-                                            "porcentajeGanancia": this.porcentajeGanancia,
-                                            "stock": this.stock,
-                                            "marca": this.marca,
-                                            "talle": this.tallesProducto, 
-                                            "url": this.imagenProducto, 
-                                            "equipo": this.equipoProducto, 
-                                            "tipo": this.tipoProducto})
-            .then(response => {
+            if (this.marca === "") {
                 swal({
-                    text: "Producto agregado a la tienda",
-                    icon: "success",
-                    button: false,
+                    text: "Elija una marca para su producto",
+                    timer: 3000,
+                    icon: "warning"
                 })
-                setTimeout(function(){
-                    location.reload()
-                }, 2500)
-            })
-            .catch(error => {
-                console.log(error.response.data)
-            })
+            }else if(this.descripcionProducto === ""){
+                swal({
+                    text: "Elija un nombre para su producto",
+                    timer: 3000,
+                    icon: "warning"
+                })
+            }else if(this.costo <= 0){
+                swal({
+                    text: "El costo del producto debe der mayor a 0",
+                    timer: 3000,
+                    icon: "warning"
+                })
+            }else if(this.porcentajeGanancia <= 0){
+                swal({
+                    text: "Indique el porcentaje de ganancia que desea obtener",
+                    timer: 3000,
+                    icon: "warning"
+                })
+            }else if(this.stock <= 0){
+                swal({
+                    text: "Indique de cuantas unidades del producto dispone",
+                    timer: 3000,
+                    icon: "warning"
+                })
+            }else if(this.tallesProducto.length < 1){
+                swal({
+                    text: "Seleccione los talles disponibles del producto",
+                    timer: 3000,
+                    icon: "warning"
+                })
+            }else if(this.nombreProducto === "Botin" && this.tipoProducto === ""){
+                swal({
+                    text: "Indique el tipo de botín",
+                    timer: 3000,
+                    icon: "warning"
+                })
+            }else if(this.imagenProducto === ""){
+                swal({
+                    text: "Seleccione una imagen para su producto",
+                    timer: 3000,
+                    icon: "warning"
+                })
+            } else {
+                axios.post("/api/productos", {
+                    "name": this.nombreProducto,
+                    "nombreProducto": this.descripcionProducto,
+                    "precioCosto": this.costo,
+                    "porcentajeGanancia": this.porcentajeGanancia,
+                    "stock": this.stock,
+                    "marca": this.marca,
+                    "talle": this.tallesProducto,
+                    "url": this.imagenProducto,
+                    "equipo": this.equipoProducto,
+                    "tipo": this.tipoProducto
+                })
+                .then(response => {
+                    swal({
+                        text: "Producto agregado a la tienda",
+                        icon: "success",
+                        button: false,
+                    })
+                    setTimeout(function () {
+                        location.reload()
+                    }, 2500)
+                })
+                .catch(error => {
+                    console.log(error.response.data)
+                })
+            }
         }
     },
     computed: {
